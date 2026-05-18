c6bda912e445f2bd2e40b2cd0726f82909c16da4
tikara <tikaradate@gmail.com>
2026-05-17 21:44:29 -0300

    Create Vulkan instance

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,46 @@
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 #include <cstdlib>
 #include <iostream>
+#include <stdexcept>
 
 static void glfw_error_callback(int error, const char *description){
     std::cerr << "GLFW error " << error << ": " << description << '\n';
+}
+
+VkInstance createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "East Shmup";
+    appInfo.applicationVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.pEngineName = "East Shmup Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(0, 1, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    uint32_t extensionCount = 0;
+    const char **extensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+    if(extensions == nullptr || extensionCount == 0){
+        throw std::runtime_error("GLFW did not return required Vulkan extensions");
+    }
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+    createInfo.enabledExtensionCount = extensionCount;
+    createInfo.ppEnabledExtensionNames = extensions;
+    createInfo.enabledLayerCount = 0;
+
+    VkInstance instance = VK_NULL_HANDLE;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if(result != VK_SUCCESS){
+        throw std::runtime_error("Failed to create Vulkan instance");
+    }
+
+    return instance;
 }
 
 int main(){
@@ -32,6 +68,9 @@ int main(){
         return EXIT_FAILURE;
     }
 
+    VkInstance instance = createInstance();
+    std::cout << "Vulkan instance created\n";
+
     while(!glfwWindowShouldClose(window)){
         glfwPollEvents();
 
@@ -41,6 +80,8 @@ int main(){
     }
 
     glfwDestroyWindow(window);
+    vkDestroyInstance(instance, nullptr);
+    
     glfwTerminate();
 
     return EXIT_SUCCESS;
